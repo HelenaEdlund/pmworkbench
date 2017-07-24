19f26f80d1e1d44f7a2fcd5d79d05dcb421d2562
Helena Edlund <helena.edlund@astrazeneca.com>
2017-07-24 11:10:00 -0400

    Create aztable.R

---
+++ aztable.R
@@ -0,0 +1,20 @@
+aztable <- funtion(dataframe, caption, label){
+  table <- 
+    dust(dataframe,
+       longtable=T,
+       hhline = T, 
+       caption = caption,
+       label = label) %>%
+    # border thickness does for some reason not work. 
+    # Trying to update latex packages on cluster to see it that solves it
+    sprinkle(part="head", bold=T, 
+             border=c("top", "bottom"), 
+             border_thickness=3, border_units="pt") %>% 
+    sprinkle(row=nrow(concByDose), border=c("bottom"), 
+             border_thickness=3, border_units="pt") %>% 
+    sprinkle_print_method("latex")
+  return(table)
+  # To fix: 
+  # automatically includes two additional linebreaks between caption and table - remove.
+  # proably good to set a default for significant digits of numeric cols
+}
