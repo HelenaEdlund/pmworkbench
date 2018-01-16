0c72b8d1ed2bb9dc152893aaa9ac56fb5b476c80
Devin Pastoor <devin.pastoor@gmail.com>
2018-01-16 10:53:36 -0500

    add package filepath to abstract system.file calls

--- R/utils.R
+++ R/utils.R
@@ -5,3 +5,11 @@
 is_dir <- function(x) {
   isTRUE(file.info(x)$isdir)
 }
+
+#' get a file or dir from a the package
+#' @param ... parameters to pass to system.file
+#' @details
+#' a light wrapper around system.file
+package_filepath <- function(...) {
+  system.file(..., package = "xpmworkbench")
+}
