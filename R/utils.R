951a61a108500e1f46aca137a22ef6d4af4e7d6e
Helena Edlund <helena.edlund@astrazeneca.com>
2018-02-22 09:39:54 -0500

    moved source_dir from main to utils

--- R/utils.R
+++ R/utils.R
@@ -5,3 +5,15 @@
 is_dir <- function(x) {
   isTRUE(file.info(x)$isdir)
 }
+
+#' source all .R files in a directory
+#' @param path directory paths
+#' @param trace Default = TRUE
+#' @export
+source_dir <- function(path, trace=TRUE, ...) {
+  for(nm in list.files(path, pattern = "[.][Rr]$")){
+    if(trace) cat(nm,":")
+    source(file.path(path, nm), ...)
+    if(trace) cat("\n")
+  }
+}
