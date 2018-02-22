523e287a67b43903db0d5c70e070a6429287d4d6
Helena Edlund <helena.edlund@astrazeneca.com>
2018-02-22 09:39:31 -0500

    moved source_dir to utils plus added 2*s08

--- inst/poppk_workflow/s00_main.R
+++ inst/poppk_workflow/s00_main.R
@@ -3,7 +3,7 @@
 # 
 # Author: 
 # Created on: 
-# Description: Make file for population PK analysis workflow
+# Description: "Make-file" for population PK analysis workflow
 # Dependencies: None
 ###################################################
 # When starting a new project, run make_project() and open the 
@@ -12,62 +12,53 @@
 # All programs that needs to be executed are "sourced" below in the order they should be 
 # executed. 
 
-# Function to source all scripts in one directory
-sourceDir <- function(path, trace=TRUE, ...) {
-  for(nm in list.files(path, pattern = "[.][Rr]$")){
-    if(trace) cat(nm,":")
-    source(file.path(path, nm), ...)
-    if(trace) cat("\n")
-  }
-}
-
 # ----------- Project setup -------------------
-sourceDir("./Scripts/Setup")
-# sourceDir("./Scripts/Functions")
+source_dir("./Scripts/Setup")
+source_dir("./Scripts/Functions")
 # tidyproject checks
 Renvironment_info()
 check_session()
 
-
 # ----------- Read in source data and set stucture for R ------------------
-source(file = file.path(scriptsDir, "s01_datasetPrep.R"))
-
+source(file = file.path(scripts_dir, "s01_dataset_preparation.R"))
 
 # The .Rmd scripts below generate a .html file with the output. 
 # If you also want graphs and tables to be outputted as separate files, 
-# set printResults to TRUE
+# set print_results to TRUE
 
 # ----------- Dataset checkout ------------------
 # Go through the script to make sure all relevant columns are included and checked
-rmarkdown::render(input=file.path("Scripts","s02_datasetReview.Rmd"))
+rmarkdown::render(input=file.path("Scripts","s02_dataset_review.Rmd"))
 
 # ----------- Summarize excluded data ------------------
-rmarkdown::render(input=file.path("Scripts","s03_summaryOfExcludedData.Rmd"), 
-                  params = list(printResults=F))
+rmarkdown::render(input=file.path("Scripts","s03_summary_excluded_data.Rmd"), 
+                  params = list(print_results=F))
 
 # ----------- Exploratory Data Analysis ------------------
-rmarkdown::render(input = file.path("Scripts", "s04_EDACovariates.Rmd"), 
-                  params = list(printResults=F))
-rmarkdown::render(input = file.path("Scripts", "s05_EDAConcTime.Rmd"), 
-                  params = list(printResults=F))
-rmarkdown::render(input = file.path("Scripts", "s06_EDAPkLinearity.Rmd"), 
-                  params = list(printResults=F))
-
-# The error: 
-# Error in unlockBinding("params", <environment>) : no binding for "params"
-# Can be ignored. It's because we need to delete "params" before the scripts is done.
+rmarkdown::render(input = file.path("Scripts", "s04_eda_covariates.Rmd"), 
+                  params = list(print_results=F))
+rmarkdown::render(input = file.path("Scripts", "s05_eda_conc_time.Rmd"), 
+                  params = list(print_results=F))
+rmarkdown::render(input = file.path("Scripts", "s06_eda_pk_linearity.Rmd"), 
+                  params = list(print_results=F))
 
 # ----------- Preparation of NONMEM datasets ------------------
-# If printCSV is true the script outputs the dataset(s) to "DerivedData", 
+# If print_csv is true the script outputs the dataset(s) to "DerivedData", 
 # otherwise it just saves the datasets and the names of the datasets s07.RData
-rmarkdown::render(input =file.path("Scripts","s07_NMDatasets.Rmd"), 
-                  params = list(printCSV=T))
+
+# " Error in unlockBinding("params", <environment>) : no binding for "params" " can be ignored. 
+# It's because we need to delete "params" before the scripts is done. Output is created anyway
+rmarkdown::render(input =file.path("Scripts","s07_nm_datasets.Rmd"), 
+                  params = list(print_csv=TRUE))
+
+# ----------- Model development ------------------
+# Execute base models 
+source(file=file.path("Scripts", "s08_base_models_execute.R"))
+# Evaluation of base models 
+rmarkdown::render(input = file.path("Scripts", "s08_base_models_evaluation.Rmd"), 
+                  params = list(print_results=F))
 
 
 
-
-
-## To be continued with model development and reporting ##
-
-
+## To be continued with covariate model development and reporting ##
 
