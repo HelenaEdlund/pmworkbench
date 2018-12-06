d200ecc22219602db1f4744f5e8215aee27bd717
Helena Edlund <helena.edlund@astrazeneca.com>
2018-12-06 13:40:42 -0500

    merged

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,22 @@
+# ------------------------
+#  Pipe
+# ------------------------
+
+#' Pipe operator
+#'
+#' See \code{magrittr::\link[magrittr]{\%>\%}} for details.
+#'
+#' @name %>%
+#' @rdname pipe
+#' @keywords internal
+#' @export
+#' @importFrom magrittr %>%
+#' @usage lhs \%>\% rhs
+NULL
+
+# ------------------------
+#  Detect if a filepath is a directory
+# ------------------------
 
 #' detect if a filepath is for a directory
 #' @param x vector of file paths
@@ -6,11 +25,16 @@ is_dir <- function(x) {
   isTRUE(file.info(x)$isdir)
 }
 
-#' source all .R files in a directory
+# ------------------------
+#  Source all .R files in a directory
+# ------------------------
+
+#' Source all .R files in a directory
 #' @param path directory paths
 #' @param trace Default = TRUE
 #' @export
 source_dir <- function(path, trace=TRUE, ...) {
+  
   for(nm in list.files(path, pattern = "[.][Rr]$")){
     if(trace) cat(nm,":")
     source(file.path(path, nm), ...)
