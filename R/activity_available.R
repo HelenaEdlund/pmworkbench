cd07efbbc5ae5da65bb7576f3cbf7641f468f509
Helena Edlund <helena.edlund@astrazeneca.com>
2019-01-31 20:29:10 +0000

    restructure and naming convention

--- R/activity_available.R
+++ R/activity_available.R
@@ -4,27 +4,27 @@
 
 #' @title Show available activities
 #' @description Show available activities
-#' @param show_devel (Logical) Show activities currently under development, Default: FALSE
+#' @param devel (Logical) Show activities currently under development, Default: FALSE
 #' @return Character string of the activities available
 #' @rdname activity_available
 #' @export 
 
-activity_available <- function(show_devel = FALSE){
+activity_available <- function(devel = FALSE){
   
-  if(!is.logical(show_devel)){
-    message(show_devel, "is not a logical. Ignored.")
-    show_devel <- FALSE
+  if(!is.logical(devel)){
+    message(devel, "is not a logical. Ignored.")
+    devel <- FALSE
   }
-  
+
   # find stable activities
-  if(!show_devel){
+  if(!devel){
     
-    activities <- dir(file.path(
-      system.file(package = "pmworkbench"), "activity_templates_stable"), recursive = F) 
+    activities <- dir(package_filepath("activity_templates_stable"), recursive = F) 
+  
   } else {
     
-    activities <- dir(file.path(
-      system.file(package = "pmworkbench"), "activity_templates_devel"), recursive = F) 
+    activities <- dir(package_filepath("activity_templates_devel"), recursive = F)
+
   }
   
   return(activities)
--- R/activity_download.R
+++ R/activity_download.R
@@ -7,6 +7,7 @@
 #' @param path path to copy to, defaults to a 'Scripts' folder
 #' @param activity_type type of activity, default poppk
 #' @param overwrite PARAM_DESCRIPTION, Default: FALSE
+#' @param devel (Logical) Get activities currently under development, Default: FALSE
 #' @return A set of template script files for the specified activity
 #' @rdname activity_download
 #' @export 
@@ -16,40 +17,51 @@
 
 activity_download <- function(path = "./Scripts",
                               activity_type = "poppk",
-                              overwrite = FALSE) {
-    
-    project_folder <- package_filepath(activity_type)
-    if (project_folder == "") {
-      stop(glue::glue("No activity detected for type:  {activity_type},
-                      check available via `activity_available()`"))
-    }
-    
-    if (dir.exists(path) && !overwrite) {
-      if(length(list_files(path))) {
-        stop_failure("Files already detected in folder ", 
-                     crayon::red(path), 
-                     ", set overwrite = TRUE to overwrite")
-      }
-    }
-    
-    if (!dir.exists(path)) {
-      mkdirp(path)
-    }
-    
-    project_files <- list_files(project_folder) %>%
-      discard(is_dir)
-    to_copy <- file.path(project_folder, project_files)
-    to_path <- file.path(path, project_files)
-    
-    to_dirs <- unique(dirname(to_path)) %>% discard(~ .x == ".") 
-    
-    # Messages for successful downloads/progress
-    purrr::map(to_dirs, mkdirp)
-    done("Directory structure created")
-
-    file.copy(to_copy, to_path)
-    done("Files copied")
-    
-    done(crayon::blue(activity_type), " files available at ", crayon::blue(path) )
-    return(TRUE) 
+                              overwrite = FALSE, 
+                              devel = FALSE) {
+  
+  # Look for activity in stable or development
+  if(!devel){
+    package_activity_folder <- 
+      package_filepath(file.path("activity_templates_stable", activity_type))
+  } else {
+    package_activity_folder <- 
+      package_filepath(file.path("activity_templates_devel", activity_type))
   }
+  
+  # make sure activity exist
+  if (package_activity_folder == "") {
+    stop(glue::glue("No activity detected for type: {activity_type},
+                      check available via `activity_available()` or try `devel=TRUE`"))
+  }
+  
+  # Check for exsisting files
+  if(dir.exists(path) && !overwrite) {
+    if(length(list_files(path))) {
+      stop_failure("Files already detected in folder ", 
+                   crayon::red(path), 
+                   ", set overwrite = TRUE to overwrite")
+    }
+  }
+  
+  if (!dir.exists(path)) {
+    mkdirp(path)
+  }
+  
+  # prepare files to copy
+  activity_files <- list_files(package_activity_folder) %>% discard(is_dir)
+  to_copy <- file.path(package_activity_folder, activity_files)
+  to_path <- file.path(path, activity_files)
+  
+  to_dirs <- unique(dirname(to_path)) %>% discard(~ .x == ".") 
+  
+  # Messages for successful downloads/progress
+  purrr::map(to_dirs, mkdirp)
+  done("Directory structure created")
+  
+  file.copy(to_copy, to_path)
+  done("Files copied")
+  
+  done(crayon::blue(activity_type), " files available at ", crayon::blue(path) )
+  return(TRUE) 
+}
